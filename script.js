010534a7ad0d1bc9dd1379e95956d01dd255ad81
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-05-24 16:47:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
+  
+  let button = document.getElementById('addTodoBtn');
+  let list = document.getElementById('todoList');
 
+button.addEventListener('click',addingtodo);
+
+  function addingtodo() {
+	let input = document.getElementById('newTodoInput').value.trim();
+	  if(input!==""){
+	 let li =document.createElement('li') ;
+	  li.innerText = input;
+     list.appendChild(li);
+	document.getElementById('newTodoInput').value="";
+	  }
+  }
